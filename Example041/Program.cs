8ac68c390ee43c2e05d0af4a9e3c8602344d5096
Berus Frager <gitmailru@yandex.ru>
2022-08-25 19:02:45 +0300

    Коммит файлов папки проекта Example041

---
+++ Example041/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41:
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+int pozitivNumber = 0;
+Console.Write("Введите длину массива: ");
+int lengthArray = Convert.ToInt32(Console.ReadLine());
+if (lengthArray > 0)
+{
+    for (int i = 0; i < lengthArray; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива из {lengthArray}: ");
+        int element = Convert.ToInt32(Console.ReadLine());
+        if (element > 0)
+        {
+            pozitivNumber = pozitivNumber + 1;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Введена неверная длина массива!!!");
+}
+Console.WriteLine($"Положительных чисел в массиве: {pozitivNumber}.");
